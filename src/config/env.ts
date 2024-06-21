80c0a73ce3e63e0fe5cc5f58c256d3ae170541d2
SushanthMarada_1807 <99113563+SushanthMarada@users.noreply.github.com>
2024-06-21 22:37:34 +0530

    final commit

---
+++ src/config/env.ts
@@ -0,0 +1,18 @@
+import Zennv from "zennv";
+import { z } from "zod";
+
+export const env = Zennv({
+    dotenv: true,
+    schema: z.object({
+        PORT: z.number().default(3006),
+        HOST: z.string().default("0.0.0.0"),
+        DATABASE_CONNECTION: z.string(),
+    }),
+});
+
+if (!env.DATABASE_CONNECTION) {
+    console.error("DATABASE_CONNECTION environment variable is required but not set.");
+    process.exit(1);
+}
+
+console.log(env);
